5af389b6cd4199f5ec72f77336a73790eb062ef6
Unknown <chrisrey95@gmail.com>
2018-03-11 00:01:15 -0400

    Add Lab 6 assigment

---
+++ Lab6/assigment.cpp
@@ -0,0 +1,31 @@
+/*
+Problem)
+1) Design a PayRoll class that has data members for an employee’s
+hourly pay rate and number of hours worked.
+2) Write a program with an array of eleven PayRoll objects.
+3) The program should read the number of hours each employee
+worked and their hourly pay rate (before tax) from a file and call
+class function to store this information in the appropriate objects.
+4) It should then call class function, once for each object, to return the
+employee’s net pay after 20% tax deduction, so this information
+can be displayed.
+a. Sample data to test this program can be found in the
+payroll.dat file located on Blackboard.
+Submission	instructions
+• Test	your	code	before	submission.
+• Add	comments	to	explain	your	solution.
+• Due	date	is	Friday 03/16/2018 @	11:59pm/
+• You	are allowed	to	submit	late	with	-2%	per	day.
+• Submission	will	not	be	available	after	Sunday March /18th/2018 @	
+11:59pm.
+• Check	rubric.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    return 0;
+}
